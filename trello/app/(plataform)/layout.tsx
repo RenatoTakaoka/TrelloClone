3228e16f383f24cec254cd239858e05efa20a156
RenatoTakaoka <renato.takaoka13@gmail.com>
2024-11-29 18:40:00 -0300

    Criado "create new board" e seu popover

--- trello/app/(plataform)/layout.tsx
+++ trello/app/(plataform)/layout.tsx
@@ -1,4 +1,5 @@
 import { ClerkProvider } from "@clerk/nextjs"
+import {Toaster} from "sonner";
 
 const PlataformLayout = ({
     children
@@ -7,6 +8,7 @@ const PlataformLayout = ({
 }) => {
     return (
         <ClerkProvider>
+            <Toaster />
             {children}
         </ClerkProvider>
     )
