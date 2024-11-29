f46fada119614c5b1e58cc51626dca5bf8ab5dde
renato takaoka <renato.takaoka13@gmail.com>
2024-11-29 13:05:31 -0300

    Versão inicial do app com tela de marketing e alguns componentes

---
+++ trello/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+
+import "./globals.css";
+import { siteConfig } from "@/config/site";
+
+
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`, 
+  },
+  description: siteConfig.description,
+  icons: [
+    {
+      url: "/logo.svg",
+      href: "logo.svg"
+    }
+  ]
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
